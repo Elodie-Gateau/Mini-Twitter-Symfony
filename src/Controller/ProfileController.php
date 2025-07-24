972a3edd1b3883984bed08698480721217820775
fournest sebastien <sebfour85@gmail.com>
2025-07-24 15:57:30 +0200

    profile & controller

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+
+
+#[Route('/profile')]
+final class ProfileController extends AbstractController
+{
+
+    #[Route('/{id}', name: 'app_profile_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        if ($user !== $this->getUser() && !$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Vous n\'avez pas la permission de voir ce profil.');
+        }
+        return $this->render('profile/show.html.twig', [
+            'profile' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($user !== $this->getUser()) {
+            throw new AccessDeniedException('Vous n\'avez pas la permission de modifier ce profil.');
+        }
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            // $this->addFlash('success', 'Votre profil a été mis à jour avec succès.');
+            return $this->redirectToRoute('app_profile_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'profile' => $user,
+            'form' => $form,
+        ]);
+    }
+}
