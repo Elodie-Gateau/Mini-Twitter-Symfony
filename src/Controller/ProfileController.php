07877ff72bb90045a226dacd16a1ab9cb76bd416
fournest sebastien <sebfour85@gmail.com>
2025-07-24 16:21:15 +0200

    profile & controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 
 
